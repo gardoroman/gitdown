fe67a42269026318124f6df1fd618715f6fc65c7
gardoroman <gardoroman@gmail.com>
2021-11-13 18:26:58 -0600

    Set up project. Can run executable with args.

---
+++ gitdown/cli.py
@@ -0,0 +1,19 @@
+import argparse
+
+def main():
+    args = parse_args()
+    args.func(args)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    commands = parser.add_subparsers(dest='command')
+    commands.required = True
+
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+def init(args):
+    print('Hello World')
